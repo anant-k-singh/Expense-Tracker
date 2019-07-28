d7d752ff018280608d3256c0eb8df52c512a04d0
anant-k-singh <anant02sep@gmail.com>
2019-07-28 15:39:59 +0530

    Barebone skeleton UI

--- app/src/main/java/in/co/dev/www/expensetracker/MainActivity.java
+++ app/src/main/java/in/co/dev/www/expensetracker/MainActivity.java
@@ -1,13 +1,53 @@
 package in.co.dev.www.expensetracker;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
+    final Context c = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.add_expense);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = LayoutInflater.from(c);
+                View dialogBox = layoutInflater.inflate(R.layout.add_expense_dialog, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(dialogBox);
+
+                final EditText userInputDialogEditText = (EditText) dialogBox.findViewById(R.id.input_expense_type);
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                // ToDo get user input here
+                            }
+                        })
+
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        dialogBox.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
+            }
+        });
     }
 }
